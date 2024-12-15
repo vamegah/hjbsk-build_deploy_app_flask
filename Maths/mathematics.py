fb2d150cde4ce3a3064b436189820723a016b684
vamegah <makvim@yahoo.com>
2024-12-14 22:16:28 -0500

    first commit

---
+++ Maths/mathematics.py
@@ -0,0 +1,17 @@
+```python
+def summation(a, b):
+    result = a + b
+    return result
+```
+
+```python
+def subtraction(a, b):
+    result = a - b
+    return result
+```
+
+```python
+def multiplication(a, b):
+    result = a * b
+    return result
+```
